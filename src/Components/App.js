ad5202c0cc1e09842991fd3ba90a2bbc7c59678e
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-03-25 01:03:00 +0100

    Add query to dependencies in useEffect hook

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,13 +7,18 @@ import GifList from './GifList';
 
 function App() {
     const [data, setData] = useState([])
+    const [query, setQuery] = useState('cats')
+
+
+
+
 
     useEffect(() => {
         axios(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=8kPPAvSPwZGTj0QyLzVO7ovMegXe79v5&q`)
             .then(response => setData(response.data.data))
             .catch(error => console.log('Error fetching and parsing data', error))
 
-    }, [])
+    }, [query])
 
 
   return (
